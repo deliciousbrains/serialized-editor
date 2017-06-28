a348d7036a14fe0b7de079e83e1a42cf0bec147f
Gilbert Pellegrom <gilbert@pellegrom.me>
2017-06-28 14:54:31 +0100

    Add tests

--- test/unit/specs/SerializedEditor.spec.js
+++ test/unit/specs/SerializedEditor.spec.js
@@ -18,4 +18,20 @@ describe('SerializedEditor.vue', () => {
 
 		assert.isEmpty(vm.generateOutput());
 	});
+
+	it('should be able to re-serialize data', () => {
+		const vm = util.mountComponent(SerializedEditor, {
+			jsonData: {"type": "s", "value": "foo bar"}
+		});
+
+		assert.equal(vm.generateOutput(), 's:7:"foo bar";');
+	});
+
+	it('should be able to handle multiple children', () => {
+		const vm = util.mountComponent(SerializedEditor, {
+			jsonData: {"type": "a", "values": [[{"type": "i", "value": "0"},{"type": "s", "value": "foo"}],[{"type": "i", "value": "1"},{"type": "s", "value": "bar"}]]}
+		});
+
+		assert.equal(vm.generateOutput(), 'a:2:{i:0;s:3:"foo";i:1;s:3:"bar";}');
+	});
 });
